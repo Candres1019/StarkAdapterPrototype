5ba4a4333b90c1df3283a9c0a6450b312adb2b40
Andres Calderon <46781564+Candres1019@users.noreply.github.com>
2021-05-26 11:19:52 -0500

    Log4j added
    Starting application log

--- StarkAdapter/src/main/java/com/wesmart/stark/StarkApplication.java
+++ StarkAdapter/src/main/java/com/wesmart/stark/StarkApplication.java
@@ -1,15 +1,18 @@
 package com.wesmart.stark;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+@Log4j2
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class StarkApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(StarkApplication.class, args);
+		log.info("Application Starting ...");
 	}
 
 }
